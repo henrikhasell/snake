8236a0c0aa736049e7ef7b90de7e836784a1bc02
Henrik Hasell <henrikhasell@gmail.com>
2016-05-15 11:40:31 +0100

    Removed blue snake.

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,6 @@ const char terrain_data[20 * 20] = {
 
 Terrain terrain(20, 20, terrain_data);
 Snake player(terrain, 10, 10);
-Snake other(terrain, 8, 15);
 
 void display()
 {
@@ -39,8 +38,6 @@ void display()
     terrain.Draw();
     glColor3f(1.0f, 0.0f, 0.0f);
     player.Draw();
-    glColor3f(0.0f, 0.0f, 1.0f);
-    other.Draw();
     glFinish();
 }
 
@@ -60,28 +57,11 @@ void special(int key, int x, int y)
 
 }
 
-void keyboard(unsigned char key, int x, int y)
-{
-    switch(key)
-    {
-		case 'w': case 'W':
-            other.SetDirection(terrain, Snake::UP);  break;
-        case 's': case 'S':
-            other.SetDirection(terrain, Snake::DOWN); break;
-        case 'a': case 'A':
-            other.SetDirection(terrain, Snake::LEFT); break;
-        case 'd': case 'D':
-            other.SetDirection(terrain, Snake::RIGHT); break;
-    }
-}
-
 void timer(int i)
 {
         glutTimerFunc(FRAME_TIME, timer, ++i);
         player.Move(terrain);
-        other.Move(terrain);
         player.Collide(terrain);
-        other.Collide(terrain);
         glutPostRedisplay();
 }
 
@@ -89,12 +69,11 @@ void timer(int i)
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
-    glutInitWindowSize(800, 800);
+    glutInitWindowSize(400, 400);
     glutCreateWindow("Simple Snake Game");
     glOrtho(0.0, 100.0, 100.0, 0.0, 1.0, -1.0);
     glutDisplayFunc(display);
     glutSpecialFunc(special);
-    glutKeyboardFunc(keyboard);
     glutTimerFunc(FRAME_TIME, timer, 0);
     glutMainLoop();
     return 0;
